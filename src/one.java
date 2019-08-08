8b8fd742c7c6522b0c4b04772e501e0b66b0f6b2
513046827@qq.com <q7141627>
2019-08-08 11:36:14 +0800

    稀疏数组

---
+++ src/one.java
@@ -0,0 +1,14 @@
+public class one {
+    public static void main(String[] args) {
+        int[][] s1=new int[9] [9];
+        s1[2][3]=3;
+        s1[3][4]=5;
+        one x=new one();
+    }
+
+    public int[][] trans(int[][] tem){
+        int row=0;
+        int[][] output=new int[row][3];
+        return null;
+    }
+}
